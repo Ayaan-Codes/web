46bf39b530159b967bd685f2ad6d16c3cacac7fd
A. CO <ayaanshaj@gmail.com>
2022-06-27 14:25:25 +0300

    Update search.js

--- corprate/js/search.js
+++ corprate/js/search.js
@@ -7,6 +7,7 @@ const products = [
     "ACO Websites & Apps",
     "ACO Enterprise",
     "ACO Development",
+    "ACO Prime",
 
 ]
 
@@ -42,6 +43,14 @@ let card_slot_content = [
       "<p>Providing development solutions to retail customers and enterprise customers to launch and maintain any asset digital or otherwise.</p>",
       "<button onclick='window.location.href=``;'>Unavailable</button>",
   ],
+
+  [
+    "<img src='https://aco.company/images/acodevelopmentbanner.png' alt='Denim Jeans' style='width:100%'>",
+    "<h1>ACO Prime</h1>",
+    "<p class='price'>Service</p>",
+    "<p>Premium service with a wide selection to cater to all needs from one person to large families. The Prime Subscription Service is one unified place that applies to every ACO service, app, and other items. This makes it so a person does not need to buy a subscription for everything they use but instead can get everything they want in one place. </p>",
+    "<button onclick='window.location.href=``;'>Unavailable</button>",
+],
 
                ];
 
